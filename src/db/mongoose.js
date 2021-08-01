2471d9dfce0c8dd9a384dcd08aa69aeebbd1890a
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-08-02 01:46:37 +0530

    database connected

---
+++ src/db/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./configuration/dev.env" });
+const connectDB = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const conn = await mongoose.connect(process.env.MONGODB_URI, {
+				useUnifiedTopology: true,
+				useFindAndModify: true,
+				useNewUrlParser: true,
+				useCreateIndex: true,
+			});
+			console.log(
+				`Mongodb connected at ${conn.connection.host}`.cyan.bold
+			);
+			resolve(conn);
+		} catch (e) {
+			console.log("MongoDB cannot be connected : ", e);
+			reject(e);
+			process.exit(1);
+		}
+	});
+};
+
+module.exports = connectDB;
