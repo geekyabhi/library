d860f6b5b44026ca145ae0ee25a01e88008c3b2a
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-08-05 17:14:51 +0530

    identity uitlity added

--- src/utilityFunctions/identities.js
+++ src/utilityFunctions/identities.js
@@ -16,4 +16,22 @@ const addIdentity = (name, email, password, role) => {
 	});
 };
 
-module.exports = { addIdentity };
+const getIdentity = (id) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			if (!id) {
+				reject({ code: 400, error: "No id detected" });
+			}
+			const identity = await Identities.findById(id);
+			if (!identity) {
+				reject({ code: 404, error: "No such identity found" });
+			}
+			resolve({ identity: identity });
+		} catch (e) {
+			console.log(e);
+			reject({ code: 500, error: e });
+		}
+	});
+};
+
+module.exports = { addIdentity, getIdentity };
