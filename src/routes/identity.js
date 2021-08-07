99459a154f4ca13b24202a6e00eef8a6ea5060f7
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-08-07 11:58:17 +0530

    :

--- src/routes/identity.js
+++ src/routes/identity.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { add, getOne } = require("../controllers/identity");
-
+const { protect } = require("../middleware/auth");
 router.route("/").post(add);
-router.route("/:id").get(getOne);
+router.route("/:id").get(protect, getOne);
 
 module.exports = router;
